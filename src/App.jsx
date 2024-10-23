4d1dcba273edd86f8f867f2cf80442855ccf1371
shochmut <skyler.hochmuth@live.com>
2024-10-22 20:28:45 -0600

    Added Material UI Dependencies. started navbar feature development.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import PrimarySearchAppBar from './components/Header.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <PrimarySearchAppBar>
+          
+      </PrimarySearchAppBar>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
